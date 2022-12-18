ca3793d1176fc9d829adb2caa5a0277044a52d44
Ahmed Arafa <arafa2982017@gmail.com>
2022-12-18 21:21:41 +0200

    edit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+function App() {
+  return (
+
+    <>
+    
+    <h1 className="title"> Welcome in React </h1>
+    <h2>
+            I am Ahmed Arafa
+    </h2>
+  
+    </>
+        
+            
+  );
+}
+
+export default App;
